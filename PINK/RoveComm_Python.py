aa4b73e235900136df1d3263c45606bd45af9fe8
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-10-02 19:00:26 -0500

    Updated RoveComm Python File

--- PINK/RoveComm_Python.py
+++ PINK/RoveComm_Python.py
@@ -19,57 +19,103 @@ print (struct.unpack('L', test_byte))
 '''
 
 ROVECOMM_PORT = 11000
-ROVECOMM_VERSION       = 1        #ToDo: Change in RC2
-ROVECOMM_HEADER_FORMAT = ">BHBHH"
-ROVCOMM_SEQ_NUM = 0x0F49
-ROVECOMM_FLAGS  = 0x00
+ROVECOMM_HEADER_FORMAT = ">HBB"
 
 ROVECOMM_SUBSCRIBE_REQUEST   = 3
 ROVECOMM_UNSUBSCRIBE_REQUEST = 4
+
+types_int_to_byte  = {
+						1:'b',
+						2:'B',
+						3:'h',
+						4:'H',
+						5:'l',
+						6:'L',
+					  }
+					  
+types_byte_to_int  = {
+						'b':1,
+						'B':2,
+						'h':3,
+						'H':4,
+						'l':5,
+						'L':6,
+					  }
+					  
+					  
+class RoveCommPacket:
+	def __init__(self, data_id=0, data_type='b', data=(), ip_octet_4= '', port = ROVECOMM_PORT):
+		self.data_id = data_id
+		self.data_type = data_type
+		self.data_count = len(data)
+		self.data = data
+		if(ip_octet_4 != ''):
+			self.ip_address = ('192.168.1.' + ip_octet_4, port)
+		else:
+			self.ip_address = ('0.0.0.0', port)
+		return
+		
+	def SetIp(self, address):
+		self.ip_address = (address, self.ip_address[1])	
+	
+	def print(self):
+		print('----------')
+		print('{0:6s} {1}'.format('ID:', self.data_id))
+		print('{0:6s} {1}'.format('Type:', self.data_type))
+		print('{0:6s} {1}'.format('Count:', self.data_count))
+		print('{0:6s} {1}'.format('IP:', self.ip_address))
+		print('{0:6s} {1}'.format('Data:', self.data))
+		print('----------')
+			
 
 class RoveCommEthernetUdp:
 	def __init__(self, port=ROVECOMM_PORT):
 		self.rove_comm_port = port
 		self.subscribers = []
-		#RC2: add last_data_type, last_data_count
 		
 		self.RoveCommSocket = socket.socket(type = socket.SOCK_DGRAM)
 		self.RoveCommSocket.setblocking(False)
 		self.RoveCommSocket.bind(("", self.rove_comm_port))
+	
+	def write(self, packet):
+		if not isinstance(packet.data, tuple):	
+			raise ValueError('Must pass data as a list, Data: ' + str(data))
+					
+		rovecomm_packet = struct.pack(ROVECOMM_HEADER_FORMAT, packet.data_id, types_byte_to_int[packet.data_type], packet.data_count)
 		
-	def write(self, data_id, data_size, data): #RC2: add data_type, data_count
-		if not isinstance(data, bytes):	
-			raise ValueError('Must pass data as a packed struct, Data: ' + str(data))
-		
-		rovecomm_packet = struct.pack(ROVECOMM_HEADER_FORMAT, ROVECOMM_VERSION, ROVCOMM_SEQ_NUM, ROVECOMM_FLAGS, data_id, data_size) + data #Todo: Debug
-		
+		for i in packet.data:
+			rovecomm_packet = rovecomm_packet + struct.pack('>' + packet.data_type, i)
+			
 		for subscriber in self.subscribers:
 			self.RoveCommSocket.sendto(rovecomm_packet, (subscriber))
 		
-	def writeTo(self, data_id, data_size, data, ip_address, port=ROVECOMM_PORT): #RC2: add data_type, data_count
-		if not isinstance(data, bytes):	
-			raise ValueError('Must pass data as a packed struct, Data: ' + str(data))
-		
-		rovecomm_packet = struct.pack(ROVECOMM_HEADER_FORMAT, ROVECOMM_VERSION, ROVCOMM_SEQ_NUM, ROVECOMM_FLAGS, data_id, data_size) + data #Todo: Debug
-		
-		self.RoveCommSocket.sendto(rovecomm_packet, (ip_address, port))
+		if(packet.ip_address != ('0.0.0.0', 0)):
+			self.RoveCommSocket.sendto(rovecomm_packet, packet.ip_address)
 		
 	def read(self):
-		try:
+		#try:
 			packet, remote_ip = self.RoveCommSocket.recvfrom(1024)
-			
 			header_size = struct.calcsize(ROVECOMM_HEADER_FORMAT)
-			rovecomm_version, seq_num, flags, data_id, data_size = struct.unpack(ROVECOMM_HEADER_FORMAT, packet[0:header_size])
+		
+			data_id, data_type, data_count = struct.unpack(ROVECOMM_HEADER_FORMAT, packet[0:header_size])
 			data = packet[header_size:]
+		
 			if(data_id == ROVECOMM_SUBSCRIBE_REQUEST):
 				if(self.subscribers.count(remote_ip) == 0):
 					self.subscribers.append(remote_ip)
 			elif(data_id == ROVECOMM_UNSUBSCRIBE_REQUEST):
 				if(self.subscribers.count(remote_ip) != 0):
 					self.subscribers.remove(remote_ip)
-			return (data_id, data_size, data)
-		
-		except:
-			return(0, 0, 0)
+			
+			data_type = types_int_to_byte[data_type]
+			data = struct.unpack('>' + data_type*data_count, data)
+			
+			returnPacket = RoveCommPacket(data_id, data_type, data, '')
+			returnPacket.ip_address = remote_ip
+			return returnPacket
+			
+		#except:
+			returnPacket = RoveCommPacket()
+			return(returnPacket)
 			
 	#def readFrom ToDo: Change to getLastIp for C++ and Python
--- PINK/Sender.py
+++ PINK/Sender.py
@@ -34,6 +34,9 @@ class Sender(QWidget):
 		self.data_data_text = QLabel('Data', self)
 		self.data_data_text.move(350, 0)
 		
+		self.ip_octet_4_text = QLabel('IP Octet 4', self)
+		self.ip_octet_4_text.move(500, 0)
+		
 		self.send1 = sendWidget(self)
 		self.send1.move(0, 10)
 		
@@ -61,20 +64,22 @@ class sendWidget(QWidget):
 		send.resize(send.sizeHint())
 		send.clicked.connect(self.sendEvent)
 		
-		self.data_id_le = QLineEdit(self)
-		self.data_size_le = QLineEdit(self)
-		self.data_data_le = QLineEdit(self)
+		self.data_id_le    = QLineEdit(self)
+		self.data_size_le  = QLineEdit(self)
+		self.data_data_le  = QLineEdit(self)
+		self.ip_octet_4_le = QLineEdit(self)
 		
 		layout=QHBoxLayout(self)
 		layout.addWidget(self.data_id_le)
 		layout.addWidget(self.data_size_le)
 		layout.addWidget(self.data_data_le)
+		layout.addWidget(self.ip_octet_4_le)
 		layout.addWidget(send)
 
 		
 	def sendEvent(self):
-		RoveComm.writeTo(int(self.data_id_le.text()), int(self.data_size_le.text()), struct.pack('>L', int(self.data_data_le.text())), '127.0.0.1')
-		print(self.data_id_le.text(), self.data_size_le.text(), self.data_data_le.text())		
+		RoveComm.writeTo(int(self.data_id_le.text()), int(self.data_size_le.text()), struct.pack('>L', int(self.data_data_le.text())), '192.168.1.'+ self.ip_octet_4_le.text())
+		print(self.data_id_le.text(), self.data_size_le.text(), self.data_data_le.text(), '192.168.1.'+ self.ip_octet_4_le.text())		
 		
 if __name__ == '__main__':
 	
