a2340700808d27d22adfe351378da9b0c1d42eda
nhsn6b <nhsn6b@umsystem.edu>
2020-01-18 00:45:42 -0600

    Cleaned up main

--- RoveComm_Tester/RoveComm_Tester.py
+++ RoveComm_Tester/RoveComm_Tester.py
@@ -1,46 +1,28 @@
-'''
-pyinstaller --windowed --onedir --icon=rover_1wP_icon.ico RoveComm_Tester.py
-'''
-
-
-#import struct
-#from PyQt5.QtCore import *
-#from PyQt5.QtWidgets import *
-#from PyQt5.QtGui import *
-#import json
-#import images_qr
-
-#import threading
-#import datetime
-#import time
-#from XboxController import *
-
 
 import os
 import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from RoveComm_Python import RoveCommPacket, RoveCommEthernetUdp
+from RoveComm_Python import RoveCommEthernetUdp
 
 from QtReciever import Reciever
 from QtSender import Sender
 
 
-rovecomm = RoveCommEthernetUdp()
-
-try:
-    os.mkdir('0-CSV Outputs')
-except:
-    pass
-
-try:
-    os.mkdir('1-Configs')
-except:
-    pass
-
-
 if __name__ == '__main__':
+
+    rovecomm = RoveCommEthernetUdp()
+
+    try:
+        os.mkdir('0-CSV Outputs')
+    except:
+        pass
+
+    try:
+        os.mkdir('1-Configs')
+    except:
+        pass
 
     app = QApplication(sys.argv)
 
